c0af6461a544230f8dfaf8d2471c635128322c51
Rame Madah <lawkira10@gmail.com>
2021-05-24 20:24:01 +0200

    contact manager 100% update 0.2

--- src/main/java/de/htwberlin/App.java
+++ src/main/java/de/htwberlin/App.java
@@ -5,15 +5,27 @@ public class App {
   public static void main(String[] args) {
 
 
+      Person Alice = new Person();
+      Alice.Addinfo("Alice ", new Contact("Megan", "Gilmore"));
+      Alice.Addinfo("Alice ", new Contact("Harry", "Fisher ", "+491253490723"));
+      Alice.showContacts();
+
+
+      Person Bob = new Person();
+      Bob.Addinfo("Bob", new Contact("Robert", "White", new Address("Warschauer Str. 1", "12345", "Berlin")));
+      Bob.Addinfo("Bob",(new Contact("Mary", "Stuart", "+491239730943", new Address("Warschauer Str. 2", "12345", "Berlin"))));
+      Bob.Addinfo("Bob",(new Contact("Bobby", "Martinez")));
+      Bob.showContacts();
+
       // Contacts of Alice
-      ContactBook.addContact(new Contact("Megan", "Gilmore"));
-      ContactBook.addContact(new Contact("Harry", "Fisher", "+491253490723"));
+    //  ContactBook.addContact(new Contact("Megan", "Gilmore"));
+      //ContactBook.addContact(new Contact("Harry", "Fisher", "+491253490723"));
 
     // Contacts of Bob
-      ContactBook.addContact(new Contact("Robert", "White", new Address("Warschauer Str. 1", "12345", "Berlin")));
-      ContactBook.addContact(new Contact("Mary", "Stuart", "+491239730943", new Address("Warschauer Str. 2", "12345", "Berlin")));
-      ContactBook.addContact(new Contact("Bobby", "Martinez"));
+      //ContactBook.addContact(new Contact("Robert", "White", new Address("Warschauer Str. 1", "12345", "Berlin")));
+      //ContactBook.addContact(new Contact("Mary", "Stuart", "+491239730943", new Address("Warschauer Str. 2", "12345", "Berlin")));
+      //ContactBook.addContact(new Contact("Bobby", "Martinez"));
 
-   // ContactBook.showContacts();
+    //ContactBook.showContacts();
   }
 }
